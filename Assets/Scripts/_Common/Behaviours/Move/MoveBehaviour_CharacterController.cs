7ba2393b4dc643ccdde17b93a03a27a23943cfc5
bugra <akbulutbugrahan@gmail.com>
2022-10-16 12:51:57 +0300

    Imported Cinemachine and Updated Movement

--- Assets/Scripts/_Common/Behaviours/Move/MoveBehaviour_CharacterController.cs
+++ Assets/Scripts/_Common/Behaviours/Move/MoveBehaviour_CharacterController.cs
@@ -7,10 +7,20 @@ namespace Common
         [SerializeField] private CharacterController _characterController = null;
 
         [SerializeField] private float _speed = 10;
+
+        [SerializeField] private bool _applyGravity = false;
         
         public void Move(Vector3 direction)
         {
             _characterController.Move(direction * (_speed * Time.deltaTime));
+        }
+
+        private void FixedUpdate()
+        {
+            if (_applyGravity)
+            {
+                _characterController.Move(Physics.gravity * Time.fixedDeltaTime);
+            }
         }
     }
 }
